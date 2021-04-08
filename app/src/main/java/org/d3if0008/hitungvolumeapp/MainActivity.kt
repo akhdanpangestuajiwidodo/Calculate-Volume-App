25351071497ff8019a10727e3e03337fdbab0399
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-04-08 19:03:35 +0700

    Code the logic in the activity class

--- app/src/main/java/org/d3if0008/hitungvolumeapp/MainActivity.kt
+++ app/src/main/java/org/d3if0008/hitungvolumeapp/MainActivity.kt
@@ -2,10 +2,38 @@ package org.d3if0008.hitungvolumeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight = findViewById(R.id.edt_heigth)
+        edtLength = findViewById(R.id.edt_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+
+        btnCalculate.setOnClickListener { calculateInput() }
+    }
+
+    private fun calculateInput(){
+        val width = edtWidth.text.toString()
+        val length = edtLength.text.toString()
+        val height = edtHeight.text.toString()
+
+        val result = width.toDouble() * length.toDouble() * height.toDouble()
+
+        tvResult.text = result.toString()
     }
 }
